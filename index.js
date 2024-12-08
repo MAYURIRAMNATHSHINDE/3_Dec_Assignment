e3f3c5e2eb2edaf980a65838c033b561fd3154da
MAYURIRAMNATHSHINDE <mayurishinde24304@gmail.com>
2024-12-08 19:24:17 +0530

    Q.no 2

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
+/**************************  Q.No.1************************** */
+
 let multiply=(a=1,b=1)=>{
     return a*b;
 }
 console.log(multiply(3,4))
+
+/*******************  Q.No 2 ******************* */
+let num=3;
+let obj={
+    square:num*num,
+    cube:num*num*num,
+
+}
+let operation=(num)=> obj;
+console.log(operation())
+
+/************************************************** */
